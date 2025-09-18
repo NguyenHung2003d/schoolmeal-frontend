b7507fff828dd7d92a92a8879de77da13d7d96be
hung <hn989422@gmail.com>
2025-09-18 22:17:46 +0700

    done homepage

--- finalproject/app/page.tsx
+++ finalproject/app/page.tsx
@@ -1,10 +1,20 @@
-import MainContent from "@/components/layouts/MainContent";
-import { FOOD_ITEMS } from "@/data/constants";
-
+import Slider from "@/sections/Slider";
+import FoodBento from "@/sections/FoodBento";
+import HeroSection from "@/sections/HeroSection";
+import Features from "@/sections/Features";
+import ParentFeedbackSection from "@/sections/FeedBack";
 export default function HomePage() {
   return (
-    <MainContent
-      foodItems={FOOD_ITEMS}
-    />
+    <main>
+      <div className="pt-24">
+        <HeroSection />
+      </div>
+      <Slider />
+      <div className="py-12">
+        <FoodBento />
+        <Features />
+        <ParentFeedbackSection />
+      </div>
+    </main>
   );
 }
