4d0786fef3022244ce899f44d26e11b150b27f84
lvt302000 <hn989422@gmail.com>
2025-10-04 11:15:16 +0700

    fix ui

--- finalproject/app/kitchen-staff/menu/page.tsx
+++ finalproject/app/kitchen-staff/menu/page.tsx
@@ -1,5 +1,5 @@
-import { MenuTab } from "@/components/kitchenstaff/menu/MenuTab";
+import { redirect } from "next/navigation";
 
 export default function MenuPage() {
-  return <MenuTab />;
+  redirect("/kitchen-staff/menu/upcoming");
 }
